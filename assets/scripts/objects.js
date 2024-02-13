27a0dbff12658f3e0560d458f9375ae3754d08c7
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-02-13 17:25:31 +0000

    Render movie list

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,6 +3,32 @@ const searchBtn = document.getElementById ('search-btn');
 
 const movies = [];
 
+const renderMovies = () => {
+  const movieList = document.getElementById('movie-list');
+
+  if (movies.length === 0){
+    movieList.classList.remove('visible');
+    return;
+  } else {
+    movieList.classList.add('visible');
+  }
+
+  movieList.innerHTML = '';
+
+  movies.forEach((movie) => {
+    const movieEl = document.createElement('li');
+    let text = movie.info.title + ' - ';
+    for (const key in movie.info) {
+      if (key !== 'title'){
+        text = text + `${key}: ${movie.info[key]}`;
+      }
+    }
+    movieEl.textContent = text;
+    movieList.append(movieEl);
+  });
+
+};
+
 const addMovieHandler = () => {
   const title = document.getElementById('title').value;
   const extraName = document.getElementById('extra-name').value;
@@ -25,7 +51,7 @@ const addMovieHandler = () => {
   };
 
   movies.push(newMovie);
-  console.log(newMovie);
+  renderMovies();
 };
 
 addMovieBtn.addEventListener('click', addMovieHandler);
